3d71dfe1c58d13fd018cc5689a4b56a0b3b362a3
Marco Júnior <mrcez4rr@gmail.com>
2025-02-19 18:19:09 -0300

    Project created

---
+++ backend/src/main/java/com/dev/emijotashop/EmijotashopApplication.java
@@ -0,0 +1,13 @@
+package com.dev.emijotashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmijotashopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmijotashopApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/dev/emijotashop/EmijotashopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.emijotashop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmijotashopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
